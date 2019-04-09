31827d21c95531f542fa7bd6681a1d1e24b311c4
Thomas Wenner <thmswenner@gmail.com>
2019-04-09 23:13:00 +0000

    Created new route /urls/:shortURL

--- express_server.js
+++ express_server.js
@@ -1,12 +1,11 @@
-var express = require("express");
-var app = express();
-var PORT = 8080; // default port 8080
+const express = require("express");
+const app = express();
+const PORT = 8080; // default port 8080
 
 //This is where we set the view engine to ejs
 app.set('view engine', 'ejs')
 
-
-var urlDatabase = {
+  const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
@@ -15,13 +14,20 @@ app.get("/", (request, response) => {
   response.send("Hello!");
 });
 
-app.get("/urls.json", (request, response) => {
-  response.json(urlDatabase);
+app.get("/urls", (request, response) => {
+  let templateVars = {urls: urlDatabase}
+  response.render('urls_index', templateVars);
+});
+
+app.get('/urls/:shortURL', (request, response) => {
+  let templateVars = {shortURL: request.params.shortURL, longURL: urlDatabase };
+  response.render('urls_show', templateVars)
 });
 
 app.get('/hello', (request, response) => {
   response.send('<html><h1>Hello World</h1></html>\n')
 })
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
