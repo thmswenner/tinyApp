3e88914fcf648c5ad3e94086c72eeac19f379df4
Thomas Wenner <thmswenner@gmail.com>
2019-04-10 01:10:42 +0000

    Added random number generator

--- express_server.js
+++ express_server.js
@@ -5,6 +5,9 @@ const PORT = 8080; // default port 8080
 //This is where we set the view engine to ejs
 app.set('view engine', 'ejs')
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
   const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -19,6 +22,10 @@ app.get("/urls", (request, response) => {
   response.render('urls_index', templateVars);
 });
 
+app.get('/urls/new', (request, response) => {
+  response.render('urls_new');
+});
+
 app.get('/urls/:shortURL', (request, response) => {
   let templateVars = {shortURL: request.params.shortURL, longURL: urlDatabase[request.params.shortURL] };
   response.render('urls_show', templateVars)
@@ -29,6 +36,22 @@ app.get('/hello', (request, response) => {
   response.send('<html><h1>Hello World</h1></html>\n')
 })
 
+app.post('/urls', (request,response) => {
+  console.log(request.body);
+  response.send('ok');
+})
+
+const generateRandomString = () => {
+  var randomStr = "";
+  var letters ="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+  for (var i = 0; i < 6; i++)
+    randomStr += letters.charAt(Math.floor(Math.random() * letters.length));
+
+  return randomStr;
+}
+
+generateRandomString()
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
