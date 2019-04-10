863b6a142c2a5e40c484f2546307577e36b98a24
Thomas Wenner <thmswenner@gmail.com>
2019-04-10 22:58:00 +0000

    Added Login and Logout using cookies

--- express_server.js
+++ express_server.js
@@ -1,10 +1,12 @@
 const express = require("express")
 const app = express()
-const  morgan = require('morgan')
+const morgan = require('morgan')
 const PORT = 8080 // default port 8080
 const bodyParser = require("body-parser")
+const cookieParser = require('cookie-parser')
 
 
+app.use(cookieParser())
 app.use(morgan('dev'))
 app.set('view engine', 'ejs')
 app.use(bodyParser.urlencoded({extended: true}))
@@ -15,55 +17,73 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 }
 
+//Login
+app.post('/login', (req, resp) => {
+  resp.cookie('username', req.body.username)
+  resp.redirect('/urls')
+})
+
+//Logout
+app.post('/logout', (req, resp) => {
+  console.log('Are you getting called?')
+  resp.clearCookie('username')
+  resp.redirect('/urls')
+})
+
 //Serves Homepage
-app.get("/urls", (req, res) => {
+app.get("/urls", (req, resp) => {
   const templateVars = {
-    urls: urlDatabase
+    urls: urlDatabase,
+    username: req.cookies["username"]
   }
-  res.render('urls_index', templateVars)
+  resp.render('urls_index', templateVars)
 })
 
 //serves create new page
-app.get('/urls/new', (req, res) => {
-  res.render('urls_new');
+app.get('/urls/new', (req, resp) => {
+  const templateVars = {
+    username: req.cookies["username"]
+  }
+  resp.render('urls_new', templateVars);
 })
 
 //serves shortURL page
-app.get('/urls/:shortURL', (req, res) => {
+app.get('/urls/:shortURL', (req, resp) => {
   const templateVars = {
     shortURL: req.params.shortURL,
-    longURL: urlDatabase[req.params.shortURL]
+    longURL: urlDatabase[req.params.shortURL],
+    username: req.cookies["username"]
   }
-  res.render('urls_show', templateVars);
+  resp.render('urls_show', templateVars);
 })
 
 //Delets URL
-app.post('/urls/:shortURL/delete', (req, res) => {
+app.post('/urls/:shortURL/delete', (req, resp) => {
   delete urlDatabase[req.params.shortURL]
-  res.redirect('/urls')
+  resp.redirect('/urls')
 })
 
 //Adds random key to longURL
-app.post('/urls', (req,res) => {
+app.post('/urls', (req,resp) => {
   const shortURL = generateRandomString()
   urlDatabase[shortURL] = req.body.longURL
-  res.redirect(`/urls/${shortURL}`)
+  resp.redirect(`/urls/${shortURL}`)
 })
-
 
 //Updates urlDatabase
-app.post('/urls/:shortURL', (req, res) => {
+app.post('/urls/:shortURL', (req, resp) => {
   console.log('Im Here')
   urlDatabase[req.params.shortURL] = req.body.longURL
-  res.redirect(`/urls/${req.params.shortURL}`)
+  resp.redirect(`/urls/${req.params.shortURL}`)
 })
 
-app.get('/urls/:shortURL', (req, res) => {
-  res.redirect(`/urls/${shortURL}`)
+app.get('/urls/:shortURL', (req, resp) => {
+  resp.redirect(`/urls/${shortURL}`)
 })
+
+
 
 //Function to create a random 6 digit string
-
 const generateRandomString = () => {
   let randomStr = ""
   const letters ="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
