849e5c698f88094125fd675642d1d35e8a8379be
Thomas Wenner <thmswenner@gmail.com>
2019-04-10 20:38:19 +0000

    Fixed bugs in the updating form

--- express_server.js
+++ express_server.js
@@ -15,37 +15,35 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 }
 
-
+//Serves Homepage
 app.get("/urls", (req, res) => {
   const templateVars = {
     urls: urlDatabase
   }
-
   res.render('urls_index', templateVars)
 })
 
-
+//serves create new page
 app.get('/urls/new', (req, res) => {
   res.render('urls_new');
 })
 
-
+//serves shortURL page
 app.get('/urls/:shortURL', (req, res) => {
   const templateVars = {
     shortURL: req.params.shortURL,
     longURL: urlDatabase[req.params.shortURL]
   }
-
   res.render('urls_show', templateVars);
 })
 
-
+//Delets URL
 app.post('/urls/:shortURL/delete', (req, res) => {
   delete urlDatabase[req.params.shortURL]
   res.redirect('/urls')
 })
 
-
+//Adds random key to longURL
 app.post('/urls', (req,res) => {
   const shortURL = generateRandomString()
   urlDatabase[shortURL] = req.body.longURL
@@ -53,6 +51,16 @@ app.post('/urls', (req,res) => {
 })
 
 
+//Updates urlDatabase
+app.post('/urls/:shortURL', (req, res) => {
+  console.log('Im Here')
+  urlDatabase[req.params.shortURL] = req.body.longURL
+  res.redirect(`/urls/${req.params.shortURL}`)
+})
+
+app.get('/urls/:shortURL', (req, res) => {
+  res.redirect(`/urls/${shortURL}`)
+})
 
 //Function to create a random 6 digit string
 
