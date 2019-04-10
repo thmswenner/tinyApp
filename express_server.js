8c949db7d356c1b39449b8c2372c460846a92d28
Thomas Wenner <thmswenner@gmail.com>
2019-04-10 18:32:47 +0000

    Added Delete Form, Button & Path

--- express_server.js
+++ express_server.js
@@ -1,59 +1,75 @@
-const express = require("express");
-const app = express();
-const PORT = 8080; // default port 8080
+const express = require("express")
+const app = express()
+const  morgan = require('morgan')
+const PORT = 8080 // default port 8080
+const bodyParser = require("body-parser")
 
-//This is where we set the view engine to ejs
+
+app.use(morgan('dev'))
 app.set('view engine', 'ejs')
+app.use(bodyParser.urlencoded({extended: true}))
 
-const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({extended: true}));
 
-  const urlDatabase = {
+const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
-};
-
-app.get("/", (request, response) => {
-  response.send("Hello!");
-});
-
-app.get("/urls", (request, response) => {
-  const templateVars = {urls: urlDatabase}
-  response.render('urls_index', templateVars);
-});
-
-app.get('/urls/new', (request, response) => {
-  response.render('urls_new');
-});
-
-app.get('/urls/:shortURL', (request, response) => {
-  const templateVars = {shortURL: request.params.shortURL, longURL: urlDatabase[request.params.shortURL] };
-  response.render('urls_show', templateVars)
-});
+}
 
 
-app.get('/hello', (request, response) => {
-  response.send('<html><h1>Hello World</h1></html>\n')
+app.get("/urls", (req, res) => {
+  const templateVars = {
+    urls: urlDatabase
+  }
+
+  res.render('urls_index', templateVars)
 })
 
-app.post('/urls', (request,response) => {
+
+app.get('/urls/new', (req, res) => {
+  res.render('urls_new');
+})
+
+
+app.get('/urls/:shortURL', (req, res) => {
+  const templateVars = {
+    shortURL: req.params.shortURL,
+    longURL: urlDatabase[req.params.shortURL]
+  }
+
+  res.render('urls_show', templateVars);
+})
+
+
+app.post('/urls/:shortURL/delete', (req, res) => {
+  delete urlDatabase[req.params.shortURL]
+  res.redirect('/urls')
+})
+
+
+app.post('/urls', (req,res) => {
   const shortURL = generateRandomString()
-  urlDatabase[shortURL] = request.body.longURL
-  response.redirect(`/urls/${shortURL}`)
+  urlDatabase[shortURL] = req.body.longURL
+  res.redirect(`/urls/${shortURL}`)
 })
+
+
+
+//Function to create a random 6 digit string
 
 const generateRandomString = () => {
-  let randomStr = "";
-  const letters ="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+  let randomStr = ""
+  const letters ="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
 
   for (var i = 0; i < 6; i++)
-    randomStr += letters.charAt(Math.floor(Math.random() * letters.length));
+    randomStr += letters.charAt(Math.floor(Math.random() * letters.length))
 
-  return randomStr;
+  return randomStr
 }
 
 generateRandomString()
 
+
+//listens for whatever the PORT is
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
-});
+  console.log(`Example app listening on port ${PORT}!`)
+})
